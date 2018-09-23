a4324e5de2115c349910efce5c0f9dc7dfce1b5f
ASUS <ASUS@DESKTOP-OTG3GSS>
2018-09-23 17:03:12 +0530

    mod 1

--- src/main/java/surendra/HelloDemo.java
+++ src/main/java/surendra/HelloDemo.java
@@ -5,6 +5,7 @@ public class HelloDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD1");
 	}
 
 }
